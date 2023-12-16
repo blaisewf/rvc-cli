edfd0aca9b6f39f40b029b3c7b34f814b584bdab
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-12-16 18:40:23 +0100

    fix validators

--- rvc/tools/validators.py
+++ rvc/tools/validators.py
@@ -1,29 +1,25 @@
 import argparse
 
 def validate_sampling_rate(value):
-    valid_sampling_rates = [32000, 40000, 48000]
-    try:
-        sampling_rate = int(value)
-        if sampling_rate in valid_sampling_rates:
-            return sampling_rate
-        else:
-            raise argparse.ArgumentTypeError(
-                f"Invalid sampling rate. Please choose from {valid_sampling_rates}"
-            )
-    except ValueError:
-        raise argparse.ArgumentTypeError("Sampling rate must be a valid integer")
+    valid_sampling = [
+        "32000",
+        "40000",
+        "48000",
+    ]
+    if value in valid_sampling:
+        return value
+    else:
+        raise argparse.ArgumentTypeError(
+            f"Invalid f0method. Please choose from {valid_sampling}"
+        )
 
 def validate_f0up_key(value):
-    try:
-        f0up_key = int(value)
-        if -12 <= f0up_key <= 12:
-            return f0up_key
-        else:
-            raise argparse.ArgumentTypeError(
-                f"f0up_key must be in the range of -12 to +12"
-            )
-    except ValueError:
-        raise argparse.ArgumentTypeError("f0up_key must be a valid integer")
+    f0up_key = int(value)
+    if -12 <= f0up_key <= 12:
+        return f0up_key
+    else:
+        raise argparse.ArgumentTypeError(f"f0up_key must be in the range of -12 to +12")
+
 
 def validate_f0method(value):
     valid_f0methods = [
