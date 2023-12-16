2e37c2e34a28df45bc82d75341d43a5d001621cf
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-12-16 13:15:02 +0100

    simplify installer

--- install.bat
+++ install.bat
@@ -7,9 +7,6 @@ set "INSTALL_ENV_DIR=%principal%\env"
 set "MINICONDA_DOWNLOAD_URL=https://repo.anaconda.com/miniconda/Miniconda3-py39_23.9.0-0-Windows-x86_64.exe"
 set "CONDA_EXECUTABLE=%CONDA_ROOT_PREFIX%\Scripts\conda.exe"
 
-for /f "delims=: tokens=*" %%A in ('findstr /b ":::" "%~f0"') do @echo(%%A
-echo.
-
 if not exist "%cd%\env.zip" (
     echo Downloading the fairseq build...
     curl -s -LJO %URL_EXTRA%/env.zip -o env.zip
@@ -33,29 +30,27 @@ if not exist "%cd%\env" (
 if exist "%cd%\env" (
     del env.zip
 ) else (
-    echo Theres a problem extracting the file please download the file and extract it manually in the applio folder
+    echo Theres a problem extracting the file please download the file and extract it manually.
     echo https://huggingface.co/IAHispano/applio/resolve/main/env.zip
     pause
     exit
 )
 
 if not exist "%CONDA_EXECUTABLE%" (
-    echo Downloading Miniconda from %MINICONDA_DOWNLOAD_URL%
+    echo Downloading Miniconda from %MINICONDA_DOWNLOAD_URL%...
     curl %MINICONDA_DOWNLOAD_URL% -o miniconda.exe
 
     if not exist "%principal%\miniconda.exe" (
-        echo Download failed trying with the powershell method
+        echo Download failed trying with the powershell method.
         powershell -Command "& {Invoke-WebRequest -Uri '%MINICONDA_DOWNLOAD_URL%' -OutFile 'miniconda.exe'}"
     )
 
-    echo Installing Miniconda to %CONDA_ROOT_PREFIX%
+    echo Installing Miniconda to %CONDA_ROOT_PREFIX%...
     start /wait "" miniconda.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%CONDA_ROOT_PREFIX%
     del miniconda.exe
 )
 
-echo Packages to install: %PACKAGES_TO_INSTALL%
 call "%CONDA_ROOT_PREFIX%\_conda.exe" create --no-shortcuts -y -k --prefix "%INSTALL_ENV_DIR%" python=3.9
-echo Conda env installed !
 
 echo Installing the dependencies...
 call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%"
