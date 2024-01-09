dcd1d753448e7e4cdb286fe10a18e1631d04baa0
Blaise <133521603+blaise-tk@users.noreply.github.com>
2024-01-09 23:12:18 +0100

    mini fix

--- rvc/lib/utils.py
+++ rvc/lib/utils.py
@@ -2,12 +2,12 @@ import ffmpeg
 import numpy as np
 
 
-def load_audio(file, sr):
+def load_audio(file, sampling_rate):
     try:
         file = file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
         out, _ = (
             ffmpeg.input(file, threads=0)
-            .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr)
+            .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sampling_rate)
             .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
         )
     except Exception as error:
