438a620c190ea931b25c03b970632f195fbe2a89
aitronssesin <aitronssesin@gmail.com>
2024-01-24 21:26:56 +0100

    Better formatted title

--- rvc/lib/utils.py
+++ rvc/lib/utils.py
@@ -1,5 +1,7 @@
 import ffmpeg
 import numpy as np
+import re
+import unicodedata
 
 
 def load_audio(file, sampling_rate):
@@ -14,3 +16,11 @@ def load_audio(file, sampling_rate):
         raise RuntimeError(f"Failed to load audio: {error}")
 
     return np.frombuffer(out, np.float32).flatten()
+
+
+def format_title(title):
+    formatted_title = unicodedata.normalize('NFKD', title).encode('ascii', 'ignore').decode('utf-8')
+    formatted_title = re.sub(r'[\u2500-\u257F]+', '', formatted_title)
+    formatted_title = re.sub(r'[^\w\s.-]', '', formatted_title)
+    formatted_title = re.sub(r'\s+', '_', formatted_title)
+    return formatted_title
